1da1f93580541285eeb6cd141717dcb75421a113
2vergent <55847410+2vergent@users.noreply.github.com>
2019-11-13 03:38:14 +0530

    Update pyjournal.py

--- pyjournal.py
+++ pyjournal.py
@@ -94,7 +94,7 @@ def encrypt(u,p):
 		if x.isdigit() and x != 0:
 			shift = int(x)
 			break
-	alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	alphabet = list(map(chr, range(97,123)))
 	for x in u:
 		for y in alphabet:
 			if x == y:
@@ -132,7 +132,7 @@ def decrypt(u,p):
 		if x.isdigit() and x != 0:
 			shift = int(x)
 			break
-	alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	alphabet = list(map(chr, range(97,123)))
 	for x in u:
 		for y in alphabet:
 			if x == y:
